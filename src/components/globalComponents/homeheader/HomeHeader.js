f16d685515efe46b780c38ce6ac66c15749fc6bd
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2022-05-10 02:46:31 +0600

    dynamic detailComponents

---
+++ src/components/globalComponents/homeheader/HomeHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeHeader = ({regularParagraph, title}) => {
+  return (
+      <div className="row ml-0 mr-0">
+            <div className="home-header">
+              <p className="regular-paragraph">{regularParagraph}</p> 
+              <h3 className="h2-title">{title}</h3>
+            </div>
+          </div> 
+
+
+
+  )
+};
+
+export default HomeHeader;
